3414300ef9a903f18b4a0307627a2e3d364c9fd8
Gotevagyok <foldesgeri@gmail.com>
2020-09-17 19:08:11 +0200

    some work done

--- Python/practise/B/app.py
+++ Python/practise/B/app.py
@@ -1,3 +1,22 @@
-kerje be az szuletesi evet
-kerje be nevet
-szamolja ki az eletkort
+"""
+
+import math
+
+a = int(input("a: "))
+b = int(input("b: "))
+c = int(input("c: "))
+D = b**2 - 4*a*c
+
+
+
+if a != 0:
+"""
+
+
+
+név = input("neved: ")
+
+nev = "martin"
+
+if nev == név:
+    print("hello martin")
--- Python/registration_system/main.py
+++ Python/registration_system/main.py
@@ -18,6 +18,7 @@ TODO:
 SOMEWHAT DONE   encorporate the encryptor and credential organiser[]
 DONE    fix newlines while updating text
 DONE    make registration work
+make deciphering images work by pasting path to json file
 add dates to enries
 """
 
@@ -31,6 +32,102 @@ make_database()
 DEBUG = True
 # IMAGE = "/home/gotevagyok/Desktop/asd.jpg" # call.addTextBox.toPlainText()
 FILES_TO_DELETE = []
+
+
+def add_timestamp(username):
+    personalDir = userFiles + username + "/"
+    today = date.today()
+    now = datetime.now()
+    datum = today.strftime("%B %d %Y")
+    timee = now.strftime("%H %M %S")
+    with open(personalDir + "dateFile.txt", "r") as PD:
+        lastDate = PD.readline()[-1]
+        lastDate = "".join(lastDate)
+    if datum in lastDate:
+        pass
+
+    entry = call.addTextBox.toPlainText() + "\n"
+    timestampedEntry = datum + "\n" + timee + ": " + entry
+
+
+def add_date():
+    today = date.today()
+    now = datetime.now()
+    datum = today.strftime("%B %d %Y")
+    hrs = now.strftime("%H")  #:%M:%S")
+    mins = now.strftime("%M")
+    call.dateLabel.setText(datum)
+    call.hrsLabel.setText(hrs)
+    call.minsLabel.setText(mins)
+
+
+def make_user_file(username):
+    os.mkdir(userFiles + "/" + username)
+    personalDir = userFiles + username + "/"
+    with open(personalDir + "dateFile.txt", "a+"):
+        pass
+    with open(username + "_file.json", "a+") as f:
+        f.write("[]")
+        shutil.move(username + "_file.json", userFiles + "/" + username)
+        pass
+
+
+def get_data(username):
+    entry = call.addTextBox.toPlainText() + "\n"
+    if call.timestampCheckBox.isChecked():
+        add_timestamp(username)
+    else:
+        encrypting(userFiles + usrName + "/" + usrName + "_file.json", entry)
+
+
+def dump_data():
+    cleanData = decrypting(userFiles + usrName + "/" + usrName + "_file.json")
+    call.storedTextBox.setPlainText(cleanData)
+    add_date()
+
+
+try:
+    os.mkdir("userFiles")
+except FileExistsError:
+    pass
+
+
+def register():  # function to register user
+    usrName = call.regUsrNameTextBox.text()
+    if in_database("database.json", usrName):  # checking wether username is taken
+        print("Username already taken.")
+    else:
+        password = call.regPasswTextBox.text()
+        password_check = call.regPasswTextBoxCheck.text()
+        if password == password_check:
+            pw, salt = encrypt(password)  # hashing and salting the password
+            update_database("database.json", usrName, pw, salt)  # adding user to the database
+            make_user_file(usrName)
+            print("Registration succesful!")
+        else:
+            print("Inconsistent passwords.")
+
+
+def login():
+    global usrName
+    usrName = call.usrNameTextBox.text()
+    if in_database("database.json", usrName):
+        password = call.passwTextBox.text()
+        try:
+            login_success = decrypt("database.json", usrName, password)
+        except KeyError:
+            login_success = False
+        if login_success:
+            print("Login succesful.")
+            add_date()
+            call.usernameLabel.setText(usrName)
+            call.addButton.clicked.connect(get_data)
+            call.refreshButton.clicked.connect(dump_data)
+        else:
+            print("Incorrect password.")
+    else:
+        print("User not found.")
+
 
 
 def debug(stuff):
@@ -48,6 +145,13 @@ def cypherImage():
     global PICNAME
     PICNAME = getImageName(IMAGE)
     hashAndSave()
+
+def deCypherImage():
+    global IMAGE
+    IMAGE = call.addTextBox.toPlainText()
+    global PICNAME
+    PICNAME = getImageName(IMAGE)
+    justShow(PICNAME)
 
 # PICNAME = getImageName(IMAGE)
 
@@ -194,9 +298,12 @@ def cleanUp(filenamesList):
     """
     n = 0
     while n < len(filenamesList):
-        os.remove(filenamesList[n])
-        debug("File removed: " + filenamesList[n])
-        n += 1
+        try:
+            os.remove(filenamesList[n])
+            debug("File removed: " + filenamesList[n])
+            n += 1
+        except Exception:
+            pass
 
 
 def hashAndSave():
@@ -212,13 +319,13 @@ def hashAndSave():
     runTime = timerEnd - timerStart
     debug("Runtime: " + str(runTime)[:6])
 
-def justShow():
+def justShow(PICNAME):
     """
     loads the hashed json in and shows it. cleans up the unneccessary files
     Fname needs to be selected by user not hardcoded !
     """
     timerStart = timer()
-    Fname = "test.jpg_ciphered.json"
+    # Fname = "test.jpg_ciphered.json"
     HASHED_IMG = PICNAME + "_ciphered.json"
     FILES_TO_DELETE = [PICNAME + "_part2.json"]
     save(decipherImage(HASHED_IMG))
@@ -229,110 +336,10 @@ def justShow():
     debug("Runtime: " + str(runTime)[:6])
 
 
-# def cypherImage():
-#     global IMAGE
-#     IMAGE = call.addTextBox.toPlainText()
-#     hashAndSave()
-
-
-def add_timestamp(username):
-    personalDir = userFiles + username + "/"
-    today = date.today()
-    now = datetime.now()
-    datum = today.strftime("%B %d %Y")
-    timee = now.strftime("%H %M %S")
-    with open(personalDir + "dateFile.txt", "r") as PD:
-        lastDate = PD.readline()[-1]
-        lastDate = "".join(lastDate)
-    if datum in lastDate:
-        pass
-
-    entry = call.addTextBox.toPlainText() + "\n"
-    timestampedEntry = datum + "\n" + timee + ": " + entry
-
-
-def add_date():
-    today = date.today()
-    now = datetime.now()
-    datum = today.strftime("%B %d %Y")
-    hrs = now.strftime("%H")  #:%M:%S")
-    mins = now.strftime("%M")
-    call.dateLabel.setText(datum)
-    call.hrsLabel.setText(hrs)
-    call.minsLabel.setText(mins)
-
-
-def make_user_file(username):
-    os.mkdir(userFiles + "/" + username)
-    personalDir = userFiles + username + "/"
-    with open(personalDir + "dateFile.txt", "a+"):
-        pass
-    with open(username + "_file.json", "a+") as f:
-        f.write("[]")
-        shutil.move(username + "_file.json", userFiles + "/" + username)
-        pass
-
-
-def get_data(username):
-    entry = call.addTextBox.toPlainText() + "\n"
-    if call.timestampCheckBox.isChecked():
-        add_timestamp(username)
-    else:
-        encrypting(userFiles + usrName + "/" + usrName + "_file.json", entry)
-
-
-def dump_data():
-    cleanData = decrypting(userFiles + usrName + "/" + usrName + "_file.json")
-    call.storedTextBox.setPlainText(cleanData)
-    add_date()
-
-
-try:
-    os.mkdir("userFiles")
-except FileExistsError:
-    pass
-
-
-def register():  # function to register user
-    usrName = call.regUsrNameTextBox.text()
-    if in_database("database.json", usrName):  # checking wether username is taken
-        print("Username already taken.")
-    else:
-        password = call.regPasswTextBox.text()
-        password_check = call.regPasswTextBoxCheck.text()
-        if password == password_check:
-            pw, salt = encrypt(password)  # hashing and salting the password
-            update_database("database.json", usrName, pw, salt)  # adding user to the database
-            make_user_file(usrName)
-            print("Registration succesful!")
-        else:
-            print("Inconsistent passwords.")
-
-
-def login():
-    global usrName
-    usrName = call.usrNameTextBox.text()
-    if in_database("database.json", usrName):
-        password = call.passwTextBox.text()
-        try:
-            login_success = decrypt("database.json", usrName, password)
-        except KeyError:
-            login_success = False
-        if login_success:
-            print("Login succesful.")
-            add_date()
-            call.usernameLabel.setText(usrName)
-            call.addButton.clicked.connect(get_data)
-            call.refreshButton.clicked.connect(dump_data)
-        else:
-            print("Incorrect password.")
-    else:
-        print("User not found.")
-
-
 call.loginButton.clicked.connect(login)
 call.registerButton.clicked.connect(register)
 call.addImageButton.clicked.connect(cypherImage)
+call.showImageButton.clicked.connect(deCypherImage)
 
 call.show()
 app.exec_()
